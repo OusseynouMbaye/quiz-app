4bf58944e013ea5c893d4cdce5126922be061532
Ouzinem <ouzinem@hotmail.com>
2025-03-21 09:44:29 -0400

    refactor: extract header component and simplify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-
-      <nav>
-        <div>
-          <p>MY Logo</p>
-        </div>
-        <div>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </div>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+vue
+<template lang="">
+  <div>
+    <Header />
+  </div>
 </template>
+<script setup>
+import Header from './components/Header.vue'
+</script>
+<style lang=""></style>
