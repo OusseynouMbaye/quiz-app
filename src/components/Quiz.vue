52109fe73012bb8438c4b0399f945fa9bb876026
Ouzinem <ouzinem@hotmail.com>
2025-03-21 10:53:53 -0400

    feat: add Progress component and integrate it into Quiz component

---
+++ src/components/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ quiz.title }}</h1>
+    <Progress :value="step" :max="quiz.questions.length - 1" />
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import Progress from './Progress.vue'
+
+defineProps({
+  quiz: Object,
+  state: String,
+})
+const step = ref(6)
+// const answer = ref('')
+</script>
