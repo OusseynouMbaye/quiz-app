1ba64cdf2cc7794589a07b02e511badb4f7e5d26
Ouzinem <ouzinem@hotmail.com>
2025-03-22 06:55:15 -0400

    feat: add Question component and integrate it into Quiz component

---
+++ src/components/Question.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="question">
+    <h3>{{ question.question }}</h3>
+    <ul>
+      <li v-for="(choice, index) in question.choices" :key="choice">
+        <label :for="`choice-${index}`">
+          <input
+            :for="`choice-${index}`"
+            name="radio"
+            type="radio"
+            v-model="answer"
+            :value="choice"
+          />{{ choice }}
+        </label>
+      </li>
+    </ul>
+    <button :disabled="!hasAnswer" @click="emits('answer', answer)">Question suivante</button>
+  </div>
+</template>
+<script setup>
+import { ref, computed } from 'vue'
+const props = defineProps({
+  question: Object,
+})
+
+const emits = defineEmits(['answer'])
+const answer = ref(null)
+const hasAnswer = computed(() => answer.value !== null)
+</script>
+<style scoped>
+.question {
+  padding-top: 2rem;
+
+  ul li {
+    list-style-type: none;
+    padding: 0;
+  }
+  button {
+    margin-left: auto;
+    display: block;
+  }
+}
+</style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,16 +2,18 @@
   <div>
     <h1>{{ quiz.title }}</h1>
     <Progress :value="step" :max="quiz.questions.length - 1" />
+    <Question :question="question" v-if="question" />
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Progress from './Progress.vue'
+import Question from './Question.vue'
 
-defineProps({
+const props = defineProps({
   quiz: Object,
   state: String,
 })
-const step = ref(6)
-// const answer = ref('')
+const step = ref(0)
+const question = computed(() => props.quiz.questions[step.value])
 </script>
